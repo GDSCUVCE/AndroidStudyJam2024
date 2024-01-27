d6281f610a23782b893fd0151a421754ee6379af
Chinmay Patil <152681687+KingCrimson-tech@users.noreply.github.com>
2024-01-27 18:09:22 +0530

    Update 3_simple_func.kt

--- Kotlin/3_simple_func.kt
+++ Kotlin/3_simple_func.kt
@@ -1,7 +1,9 @@
 // Create a function called greet that takes a name as a parameter and prints a greeting message.
-
+ 
 // create the greet function here
-
+fun greet(name: String){
+	println("Hi $name!")
+		}
 
 //main function
 fun main () {
