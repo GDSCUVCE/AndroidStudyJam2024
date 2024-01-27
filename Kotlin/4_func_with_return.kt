2d36cabfea33e4135a2df9cac2f18b2fdbdcb0e9
Chinmay Patil <152681687+KingCrimson-tech@users.noreply.github.com>
2024-01-27 18:12:54 +0530

    Update 4_func_with_return.kt

--- Kotlin/4_func_with_return.kt
+++ Kotlin/4_func_with_return.kt
@@ -1,6 +1,6 @@
 // Create a function called calculateSquare that takes an Int parameter and returns its square.
 
-fun calculateSquare() {       // correct the function
+fun calculateSquare(side: Int): Int{       // correct the function
     return number * number
 }
 
